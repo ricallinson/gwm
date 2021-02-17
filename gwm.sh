3a16dd52c3d0a4bef3dc125415b9c176b526528f
Ric Allinson <capecodehq@yahoo.co.uk>
2021-02-16 16:54:17 -0800

    Fuck Go Modules
    I don't like change...

--- gwm.sh
+++ gwm.sh
@@ -41,7 +41,6 @@ gwm() {
         echo "    gwm help                    Show this message"
         echo "    gwm here                    Set the current directory as the Go workspace"
         echo "    gwm current                 Display currently activated workspace directory"
-        echo "    gwm link                    Link an external source repository into this workspace"
         echo "    gwm version                 The version of Go Workspace Manager installed"
         echo
     ;;
@@ -65,6 +64,7 @@ gwm() {
         gwm_gvm_use
         export GOPATH=$wPath
         export PATH=$PATH:$GOPATH/bin
+        export GO111MODULE="off"
 
         echo
         echo "Workspace set to $GOPATH"
@@ -83,35 +83,5 @@ gwm() {
         echo
     ;;
 
-    "link" )
-        if [ -z $2 ]; then
-            echo
-            echo "You must provide a path to a Go module"
-            echo
-            return 0
-        fi
-        local wPath
-        wPath=$(gwm_find_src $PWD)
-        if [ $wPath == "/src" ]; then
-            echo
-            echo "This command must be run in a Go workspace"
-            echo
-            return 0
-        fi
-        rPath=$(gwm_find_src $2)
-        if [ $rPath == "/src" ]; then
-            echo
-            echo "Could not find a Go module at $2"
-            echo
-            return 0
-        fi
-        mPath=${2#$rPath} # Remove the root path so we have just the module path.
-        mkdir -p $wPath${mPath%/*} # Make the directory path without the last item.
-        ln -s -f $2 $wPath$mPath # Symlink the module into the current workspace.
-
-        echo
-        echo "Linked module $mPath"
-        echo
-    ;;
     esac
 }
